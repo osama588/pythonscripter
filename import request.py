fb615b2b677b11dcd322bcc186264c617ba07f6a
Osama Farooq <khanusama588822743@gmail.com>
2023-12-01 01:08:42 +0500

    First commit

--- import request.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
-import json
-
-base_url = 'https://www.zameen.com/Homes/Karachi-2-1.html'  # Replace with the website URL you want to scrape
-response = requests.get(base_url)
-response = response.content
-soup = BeautifulSoup(response, 'html.parser')
--- string1.py
+++
@@ -1,4 +0,0 @@
-python_students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
-python_students.sort()
-print(python_students)
-           
